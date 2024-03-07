2ef9bf5136fcaeaa3df221e5e7f7142cff556be3
creative-slim <sa@creative-directors.com>
2024-03-07 10:28:26 +0100

    setup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Creative Developer';
+  const name = 'Creative Developer : Dashboard';
   greetUser(name);
 });
